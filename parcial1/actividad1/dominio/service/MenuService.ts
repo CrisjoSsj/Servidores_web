69955b03a29a2adb61d3bb3c22d04755712efd28
sparkRGT <joustinalonzo@hotmail.com>
2025-09-27 09:35:25 -0500

    dominio 1.1

---
+++ parcial1/actividad1/dominio/service/MenuService.ts
@@ -0,0 +1,27 @@
+import { IMenu } from "../interface/Menu";
+
+export class MenuService {
+    private menus: IMenu[] = [];
+    private nextId: number = 1;
+
+    agregarMenu(menu: Omit<IMenu, 'id_menu'>): IMenu {
+        const nuevoMenu: IMenu = { id_menu: this.nextId++, ...menu };
+        this.menus.push(nuevoMenu);
+        return nuevoMenu;
+    }   
+    obtenerMenus(): IMenu[] {
+        return this.menus;
+    }   
+    buscarMenuPorId(id: number): IMenu | undefined {
+        return this.menus.find(menu => menu.id_menu === id);
+    }
+    eliminarMenu(id: number): void {
+        this.menus = this.menus.filter(menu => menu.id_menu !== id);
+    }
+    actualizarMenu(id: number, datosActualizados: Partial<IMenu>): void {
+        const menu = this.buscarMenuPorId(id);
+        if (menu) {
+            Object.assign(menu, datosActualizados);
+        }
+    }
+}
