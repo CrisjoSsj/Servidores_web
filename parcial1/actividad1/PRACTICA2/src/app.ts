9d0dd069876392be9770308623c2516201ad376a
sparkRGT <joustinalonzo@hotmail.com>
2025-10-03 14:11:33 -0500

    config

--- parcial1/actividad1/PRACTICA2/src/app.ts
+++ parcial1/actividad1/PRACTICA2/src/app.ts
@@ -2,4 +2,13 @@ import { ClienteService } from "./service/clineteService";
 
 const clienteService = new ClienteService();
 
-clienteService.agregarCliente
+clienteService.agregarCliente({
+    id_cliente: 1,
+    nombre: "Juan",
+    correo: "Perez@gmail.co",
+    telefono: "123456789"
+});
+clienteService.obtenerClientes().forEach((cliente) => {
+    console.log(`ID: ${cliente.id_cliente}, Nombre: ${cliente.nombre}, Correo: ${cliente.correo}, Teléfono: ${cliente.telefono}`);
+}
+);
